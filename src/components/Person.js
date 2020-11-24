b0739cad8bbb8303bb5eaa6671cc8d8edac2a478
hall17 <halilakgun17@gmail.com>
2020-11-24 08:34:03 +0300

    react-query-app

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Person = ({person}) => {
+  return (
+    <div className="card">
+      <h3>{person.name}</h3>
+      <p>Gender - {person.gender}</p>
+      <p>Birth Year - {person.birth_year}</p>
+    </div>
+  )
+}
